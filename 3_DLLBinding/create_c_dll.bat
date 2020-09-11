8f0b13ea4f4d26fd841cbb955256f8027661de37
Bagus Nurtomo <bagusthanatos@gmail.com>
2020-09-11 07:15:46 +0700

    Rename sum to multiply

--- 3_DLLBinding/create_c_dll.bat
+++ 3_DLLBinding/create_c_dll.bat
@@ -3,4 +3,4 @@ REM -LD tells the compiler to create a dll instead, also implies -MT
 REM see: https://docs.microsoft.com/en-us/cpp/build/reference/md-mt-ld-use-run-time-library?view=vs-2019
 REM -TC tells the compiler to treat files as C files
 REM TODO(Bagus): Can we reduce dll size here?
-cl -nologo -TC -LD -O2 sum.cpp /link -incremental:no -EXPORT:c_multiply
+cl -nologo -TC -LD -O2 multiply.cpp /link -incremental:no -EXPORT:c_multiply
