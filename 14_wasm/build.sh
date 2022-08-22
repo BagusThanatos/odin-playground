22c33166363ac9f10645e71ae3b642699ce8050e
Bagus Nurtomo <bagusthanatos@gmail.com>
2022-08-22 19:40:12 +0700

    fix build for linux

--- 14_wasm/build.sh
+++ 14_wasm/build.sh
@@ -1,2 +1,2 @@
 mkdir -p bin/
-~/personal/Odin/odin build main.odin -target:js_wasm32 -out:bin/odin-wasm.wasm -vet -opt:2 -show-timings
+~/personal/Odin/odin build ./ -target:js_wasm32 -out:bin/odin-wasm.wasm -vet -opt:2 -show-timings
